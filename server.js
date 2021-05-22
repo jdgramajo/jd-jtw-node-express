e50837adb9745a0b32f0b3955a999064774f8dbd
JD <jdgramajo@gmail.com>
2021-05-22 10:10:06 -0600

    Adds express and cors.

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+// Server related code
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+var corsOptions = {
+  origin: 'http://127.0.0.1:8080/*'
+}
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to JWTing!' });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running in port ${PORT}`);
+});
+
+// Persistence Related code
 const db = require('./models');
 
 db.sequelize
