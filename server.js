938e27fabfa1f8bf119eb0ea8178b76791fc5c91
JD <jdgramajo@gmail.com>
2021-07-29 13:07:21 -0600

    Enables HTTPS and a script for running it locally.

--- server.js
+++ server.js
@@ -13,10 +13,22 @@ app.get("/", (req, res) => {
 require("./routes/auth")(app);
 require("./routes/user")(app);
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server running in port ${PORT}`);
-});
+const PORT = process.env.PORT || 443;
+console.log(`${process.env.ENABLE_HTTPS}`);
+const HTTPS = process.env.ENABLE_HTTPS === "true";
+if (HTTPS) {
+  const fs = require("fs");
+  const key = fs.readFileSync("./.vscode/certs/key.pem");
+  const cert = fs.readFileSync("./.vscode/certs/cert.pem");
+  const https = require("https");
+  https.createServer({ key, cert }, app).listen(PORT, () => {
+    console.log(`HTTPS server running in port ${PORT}`);
+  });
+} else {
+  app.listen(PORT, () => {
+    console.log(`HTTP server running in port ${PORT}`);
+  });
+}
 
 // Persistence Related code
 const db = require("./models");
